e978988bd9e4ba6006c4e886eb92bd0a2b85d0e7
Diego Goldszein <diego.goldszein@codesmithdev.com>
2024-08-10 11:21:02 -0300

    time fix

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -6,41 +6,59 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-
-
 const secretKey = process.env.SECRET_KEY!;
 const hashedKey = hashSecret(secretKey);
-//console.log('Hashed Secret:', hashedKey);
 
 test.describe('API Tests from Excel Data', () => {
-    const filePath = 'testData/Datos-pruebas(2).xlsx'; 
-    const testDataIterator = readExcel(filePath); 
-    
-    let row = testDataIterator.next(); 
-  
-    // Recorre cada fila usando el iterador
-    while (!row.done) {
-      const [id, name, abilities] = row.value;
-  
-      test(`API Test for ${name || id}`, async ({}) => {
-        console.log('Hashed Secret:', hashedKey);
-  
-        // Obtén los datos del Pokémon
-        const pokemon = await getPokemonData(id || name);
-  
-        // Verifica el ID y nombre del Pokémon
-        expect(pokemon.id).toBe(id);
-        expect(pokemon.name).toBe(name.toLowerCase());
-  
-        // Asegúrate de que las habilidades sean arrays de strings
-        const expectedAbilities = abilities.split(',').map((ability: string) => ability.toLowerCase().trim());
-        const actualAbilities = pokemon.abilities.map((abilityObj: any) => abilityObj.ability.name.toLowerCase());
-  
-        // Valida que las habilidades del Pokémon contengan las habilidades esperadas
-        expect.soft(actualAbilities).toEqual(expect.arrayContaining(expectedAbilities));
-      });
-  
-      // Avanza a la siguiente fila
-      row = testDataIterator.next();
-    }
-  });
+  const filePath = 'testData/Datos-pruebas(2).xlsx'; 
+  const testDataIterator = readExcel(filePath); 
+
+  let row = testDataIterator.next(); 
+
+  while (!row.done) {
+    const [id, name, abilities] = row.value;
+
+    test(`API Test for ${name || id}`, async ({}) => {
+      // Loguea la clave secreta encriptada antes de cada test
+      console.log('Hashed Secret:', hashedKey);
+      const startTime = Date.now();
+
+      
+      const pokemon = await getPokemonData(id || name);
+
+      
+      if (!pokemon) {
+        console.error(`No se encontraron datos para el Pokémon con id o nombre: ${id || name}`);
+        return; 
+      }
+
+      const responseTime = Date.now() - startTime;
+      expect(responseTime).toBeLessThan(10000); 
+
+      
+      expect(pokemon.id).toBeDefined();
+      expect(pokemon.name).toBeDefined();
+      expect(pokemon.abilities).toBeInstanceOf(Array);
+
+      
+      expect(pokemon.id).toBe(id);
+      expect(pokemon.name.toLowerCase()).toBe(name.toLowerCase());
+
+      
+      const expectedAbilities = abilities.split(',').map((ability: string) => ability.toLowerCase().trim());
+      const actualAbilities = pokemon.abilities.map((abilityObj: any) => abilityObj.ability.name.toLowerCase());
+
+      
+      expect.soft(actualAbilities).toEqual(expect.arrayContaining(expectedAbilities));
+      // console.log(actualAbilities)
+      // console.log(expectedAbilities)
+      // console.log(pokemon.id)
+      // console.log(pokemon.name)
+      
+      const endTime = new Date();
+      console.log(`Test finalizado a las: ${endTime}`);
+    });
+
+    row = testDataIterator.next();
+  }
+});
